7aaf6d9a5bf7efab3f585bf45434168aa27be742
Lugan2004 <visagielugan42@gmail.com>
2024-05-07 13:38:08 +0200

    fixed calculation for distance

--- index.js
+++ index.js
@@ -7,24 +7,27 @@
 
 // Given Parameters
 const velocityInKmh = 10000; // velocity (km/h)
+const velocityInMetersPerSecond = velocityInKmh / 3.6; // velocity (m/s)
 const spacecraftAcceleration = 3; // acceleration (m/s^2)
 const timeInSeconds = 3600; // seconds (1 hour)
 const initialDistance = 0; // distance (km)
-const IntialAmountOfFuel = 5000; // remaining fuel (kg)
+const initialAmountOfFuel = 5000; // remaining fuel (kg)
 const fuelBurnRate = 0.5; // fuel burn rate (kg/s)
 
 
-const newDistance = d + (vel*time) //calcultes new distance
-const remainingFuel = fbr*time //calculates remaining fuel
-const newVelocity = calcNewVel(acc, vel, time) //calculates new velocity based on acceleration
+const newDistance = initialDistance + (velocityInMetersPerSecond*timeInSeconds) //calcultes new distance
+const finalDistanceInKm = newDistance/1000 //new distance
+const remainingFuel = fuelBurnRate*timeInSeconds //calculates remaining fuel
+
 
 // Pick up an error with how the function below is called and make it robust to such errors
-calcNewVel = (velocityInKmh, spacecraftAcceleration, timeInSeconds) => { 
+const calcNewVel = (velocityInKmh, spacecraftAcceleration, timeInSeconds) => { 
   return velocityInKmh + (spacecraftAcceleration*timeInSeconds)
 }
+const newVelocity = calcNewVel(spacecraftAcceleration, velocityInKmh, timeInSeconds) //calculates new velocity based on acceleration
 
 console.log(`Corrected New Velocity: ${newVelocity} km/h`);
-console.log(`Corrected New Distance: ${newDistance} km`);
+console.log(`Corrected New Distance: ${finalDistanceInKm} km`);
 console.log(`Corrected Remaining Fuel: ${remainingFuel} kg`);
 
 

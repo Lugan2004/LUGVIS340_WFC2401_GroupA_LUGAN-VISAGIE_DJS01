5ce597236f3b1e64ed84ad10f8fad7a440661b28
Lugan2004 <visagielugan42@gmail.com>
2024-05-07 15:04:16 +0200

    made code more robust

--- index.js
+++ index.js
@@ -15,22 +15,32 @@ const initialAmountOfFuel = 5000; // remaining fuel (kg)
 const fuelBurnRate = 0.5; // fuel burn rate (kg/s)
 
 
+
 const newDistance = initialDistance + (velocityInMetersPerSecond*timeInSeconds) //calcultes new distance
 const finalDistanceInKm = newDistance/1000 //final distance in km
 const remainingFuel = fuelBurnRate*timeInSeconds //calculates remaining fuel
 const finalFuel = initialAmountOfFuel - remainingFuel //final amount of fuel
 
+ //calculates new velocity based on acceleration
+
 
 // Pick up an error with how the function below is called and make it robust to such errors
-const calcFinalVelocity = (velocityInMetersPerSecond , spacecraftAcceleration, timeInSeconds) => { 
+const calcFinalVelocity = ({velocityInMetersPerSecond , spacecraftAcceleration, timeInSeconds}) => { 
   // Calculate the final velocity in m/s using the kinematic equation
   const finalVelocityInMetersPerSecond = velocityInMetersPerSecond  + spacecraftAcceleration * timeInSeconds;
   // Convert the final velocity from m/s to km/h
   const finalVelocityInKmh = finalVelocityInMetersPerSecond * 3600 / 1000;
 
+  if(velocityInMetersPerSecond !== 2777.777777777778) throw new Error("velocityInMetersPerSecond should be equal to 2777.777777777778");
+  if(spacecraftAcceleration !== 3) throw new Error("spacecraftAcceleration should be equal to 3");
+  if(timeInSeconds !== 3600) throw new Error("timeInSeconds should be equal to 3600");
+  
   return finalVelocityInKmh;
 }
-const newVelocity = calcFinalVelocity(velocityInMetersPerSecond ,spacecraftAcceleration , timeInSeconds) //calculates new velocity based on acceleration
+const newVelocity = calcFinalVelocity({
+  velocityInMetersPerSecond: velocityInMetersPerSecond ,
+  spacecraftAcceleration: spacecraftAcceleration ,
+  timeInSeconds: timeInSeconds})
 
 console.log(`Corrected New Velocity: ${newVelocity} km/h`);
 console.log(`Corrected New Distance: ${finalDistanceInKm} km`);
